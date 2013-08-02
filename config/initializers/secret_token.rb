a262f0672a6784dbdd0eedc7c5372757e95fe532
DrTr <dmytromuravskyi@gmail.com>
2013-08-02 19:56:10 +0300

    Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GalCivBoardGame::Application.config.secret_key_base = '475ae15b0ca2b13a6a6fecea254b98077074f85f0bfcbb9a166bcaffd787854db1094653e83239d782d9e61f7b91cb6bda58e1280a02ca4902a1cb2694362140'
