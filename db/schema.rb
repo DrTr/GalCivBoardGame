17ef424255d1d5394de9a5302d8e7354b2cb5eea
DrTr <dmytromuravskyi@gmail.com>
2013-08-08 21:04:01 +0300

    solving heroku issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130807161423) do
+ActiveRecord::Schema.define(version: 20130808180141) do
 
   create_table "messages", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "url_title"
